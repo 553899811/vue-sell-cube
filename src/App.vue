d1073e94f625b7e87c095468f59bc04d43d00d62
张勇 <yong.zhang@dmall.com>
2020-12-12 15:05:15 +0800

    be-ui

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <cube-button style="width:120px;">1234</cube-button>
   </div>
 </template>
 
